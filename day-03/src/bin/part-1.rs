d4a2553ea22632b140739cf08cc951eca56d71d9
Simo Soini <simo@capscience.fi>
2023-12-03 17:09:18 +0200

    Added files for day 3.

---
+++ day-03/src/bin/part-1.rs
@@ -0,0 +1,4 @@
+/// Day 3: Gear Ratios Part 1
+fn main() {
+    let _input = include_str!("../../input.txt");
+}
---
+++ day-03/src/bin/part-2.rs
@@ -0,0 +1,5 @@
+/// Day 3: Gear Ratios, Part 2
+
+fn main() {
+    let _input = include_str!("../../input.txt");
+}
