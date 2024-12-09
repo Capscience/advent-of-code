aa46fa58a47c5455af43f4f4716c94ff02bfe84a
Simo Soini <caps@capscience.fi>
2024-12-09 22:12:55 +0200

    Add 2024 day 9 solutions.

--- 2024/day-09/src/main.rs
+++ 2024/day-09/src/main.rs
@@ -10,47 +10,99 @@ fn main() {
         .collect();
 
     let start_1 = Instant::now();
-    println!("Part 1: {}, {:?}", "part_1()", start_1.elapsed());
+    println!("Part 1: {}, {:?}", part_1(&input), start_1.elapsed());
     let start_2 = Instant::now();
-    println!("Part 2: {}, {:?}", "part_2()", start_2.elapsed());
+    println!("Part 2: {}, {:?}", part_2(&input), start_2.elapsed());
 }
 
 fn part_1(input: &Vec<u8>) -> usize {
-    eprintln!("Start part 1");
-    let mut checksum: usize = 0;
+    let mut disk = get_disk(input);
+    let mut ptr = 0;
+    let mut tail_ptr = disk.len() - 1;
+    let mut checksum = 0;
 
-    let mut blocks_reversed = input.iter().step_by(2).enumerate().rev();
-    let (mut last_block_id, mut last_block_len) = blocks_reversed.next().unwrap();
-    let mut last_index: usize = input.iter().map(|n| *n as usize).sum();
-    last_index -= 1; // Prevent index out of range
-    let mut disk_index: usize = 0;
-    let mut main_iterator = input.windows(2).step_by(2).enumerate();
-
-    eprintln!("Start while let");
-    while let Some((block_id, [block_len, empty_len])) = main_iterator.next() {
-        let mut next_index = disk_index + *block_len as usize;
-        for i in disk_index..next_index {
-            dbg!(i, block_id);
-            checksum += i * block_id;
-        }
-        disk_index = next_index;
-        next_index = disk_index + *empty_len as usize;
-        for i in disk_index..next_index {
-            if last_index <= i {
-                return checksum;
+    while ptr <= tail_ptr {
+        if let Some(id) = disk[ptr] {
+            checksum += ptr * id;
+            ptr += 1;
+        } else {
+            disk.swap(ptr, tail_ptr);
+            checksum += ptr * disk[ptr].unwrap();
+            ptr += 1;
+            while disk[tail_ptr].is_none() && ptr <= tail_ptr {
+                tail_ptr -= 1;
             }
-            if *last_block_len == 0 {
-                (last_block_id, last_block_len) = blocks_reversed.next().unwrap();
-            }
-
-            dbg!(i, last_block_id);
-            checksum += i * last_block_id;
-            last_index -= 1;
         }
-        disk_index = next_index;
     }
-
     checksum
+}
+
+fn part_2(input: &Vec<u8>) -> usize {
+    let mut disk = get_disk(input);
+    let mut tail_ptr = disk.len() - 1;
+    let mut current_file = disk[tail_ptr];
+    let mut current_file_len = 0;
+
+    while 0 < tail_ptr {
+        // Find current file location and length
+        while disk[tail_ptr] == current_file && tail_ptr > 0 {
+            tail_ptr -= 1;
+            current_file_len += 1;
+        }
+
+        // Search for a spot
+        let mut first_none = None;
+        let mut ptr = 0;
+        while ptr <= tail_ptr {
+            if disk[ptr].is_none() {
+                if first_none.is_none() {
+                    first_none = Some(ptr);
+                }
+                // Found a spot, move the whole file
+                if ptr + 1 - first_none.unwrap() >= current_file_len {
+                    for i in 0..current_file_len {
+                        disk.swap(first_none.unwrap() + i, tail_ptr + 1 + i);
+                    }
+                    break;
+                }
+            } else {
+                first_none = None;
+            }
+            ptr += 1;
+        }
+
+        // Find next file
+        while disk[tail_ptr].is_none() && tail_ptr > 0 {
+            tail_ptr -= 1;
+        }
+        current_file = disk[tail_ptr];
+        current_file_len = 0;
+    }
+    checksum(&disk)
+}
+
+fn checksum(disk: &[Option<usize>]) -> usize {
+    let mut checksum = 0;
+    for (i, block) in disk.iter().enumerate() {
+        if let Some(id) = block {
+            checksum += id * i;
+        }
+    }
+    checksum
+}
+
+fn get_disk(input: &Vec<u8>) -> Vec<Option<usize>> {
+    let mut disk: Vec<Option<usize>> = Vec::new();
+    for (id, block) in input.iter().enumerate() {
+        for _ in 0..*block {
+            if id % 2 == 0 {
+                disk.push(Some(id / 2));
+            } else {
+                disk.push(None);
+            }
+        }
+    }
+    disk
 }
 
 #[cfg(test)]
@@ -60,6 +112,20 @@ mod tests {
     #[test]
     fn test_part_1() {
         let input = vec![1, 2, 3, 4, 5];
-        assert_eq!(part_1(&input), 61);
+        assert_eq!(part_1(&input), 60);
+        let input: Vec<u8> = "2333133121414131402"
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect();
+        assert_eq!(part_1(&input), 1928);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input: Vec<u8> = "2333133121414131402"
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect();
+        assert_eq!(part_2(&input), 2858);
     }
 }
