73234ed50a432f589f61dfe73c9cd7a3bb45b0b9
Simo Soini <caps@capscience.fi>
2024-12-11 11:10:27 +0200

    Add 2024 day 11 solutions.

---
+++ 2024/day-11/src/main.rs
@@ -0,0 +1,61 @@
+use std::{collections::HashMap, fs, path::Path, time::Instant};
+
+fn main() {
+    let path = Path::new("input.txt");
+    let input = fs::read_to_string(path)
+        .expect("Cannot solve without input!")
+        .split_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
+
+    let start_1 = Instant::now();
+    println!("Part 1: {}, {:?}", solve(&input, 25), start_1.elapsed());
+    let start_2 = Instant::now();
+    println!("Part 2: {}, {:?}", solve(&input, 75), start_2.elapsed());
+    let start_3 = Instant::now();
+    println!(
+        "Part 3 (just for fun): {}, {:?}",
+        solve(&input, 255),
+        start_3.elapsed()
+    );
+}
+
+fn solve(input: &[u64], depth: u8) -> usize {
+    let mut cache: HashMap<(u64, u8), usize> = HashMap::new();
+    let mut length = 0;
+    for stone in input {
+        length += blink(*stone, depth, &mut cache);
+    }
+    length
+}
+
+fn blink(stone: u64, depth: u8, cache: &mut HashMap<(u64, u8), usize>) -> usize {
+    if depth == 0 {
+        return 1;
+    }
+    if let Some(len) = cache.get(&(stone, depth)) {
+        return *len;
+    }
+    let result = if stone == 0 {
+        blink(1, depth - 1, cache)
+    } else if (stone.checked_ilog10().unwrap_or(0) + 1) % 2 == 0 {
+        let number_string = stone.to_string();
+        let (a, b) = number_string.split_at(number_string.len() / 2);
+        blink(a.parse().unwrap(), depth - 1, cache) + blink(b.parse().unwrap(), depth - 1, cache)
+    } else {
+        blink(stone * 2024, depth - 1, cache)
+    };
+    cache.insert((stone, depth), result);
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve() {
+        let stones = vec![125, 17];
+        assert_eq!(solve(&stones, 25), 55312);
+    }
+}
